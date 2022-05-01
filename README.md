b85dcad2ee14d38eadf4886137e4ab1c6a6aa566
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-05-01 20:20:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Console Chat Application Using Mediator Pattern In java.
+
+# Send and Receive Messages.
+
+![output](output/screenshot.png)
---
+++ src/mediator/IChatRoom.java
@@ -0,0 +1,7 @@
+package mediator;
+
+public interface IChatRoom {
+    public void sendMessage(String message, String userId);
+
+    void addUser(User user);
+}
